637d2d33dc8be744193bf327d72f24c56be99f3b
YaredFall <yaredfall@gmail.com>
2026-05-25 21:48:17 +0700

    refactor: unify Variant and SlotVariant types into RecipeVariant

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -3,9 +3,11 @@ export type ClassValue = string | null | undefined;
 type BooleanStringToBoolean<T> = T extends "true" | "false" ? boolean : T;
 
 export type VariantsDefinition = Record<string, Record<string, string>>;
-export type Variant<V extends VariantsDefinition> = { [K in keyof V]?: BooleanStringToBoolean<keyof V[K]> };
 export type SlotVariantsDefinition<S extends string = string> = Record<string, Record<string, { [K in S]?: string }>>;
-export type SlotVariant<SV extends SlotVariantsDefinition> = { [K in keyof SV]?: BooleanStringToBoolean<keyof SV[K]> };
+
+export type RecipeVariant<V extends VariantsDefinition | SlotVariantsDefinition> = {
+    [K in keyof V]?: BooleanStringToBoolean<keyof V[K]>;
+};
 
 export type VariantKeys<V extends VariantsDefinition | SlotVariantsDefinition> = Array<keyof V>;
 export type VariantMap<V extends VariantsDefinition | SlotVariantsDefinition> = { [K in keyof V]: Array<keyof V[K]> };
@@ -14,28 +16,28 @@ export type Recipe<V extends VariantsDefinition = VariantsDefinition> = {
     className?: string;
     base?: string;
     variants?: V;
-    defaultVariants?: Variant<V>;
+    defaultVariants?: RecipeVariant<V>;
 };
 
 export type SlotRecipe<S extends string = string, SV extends SlotVariantsDefinition<S> = SlotVariantsDefinition<S>> = {
     className?: string;
     base?: Record<S, string>;
     variants?: SV;
-    defaultVariants?: SlotVariant<SV>;
+    defaultVariants?: RecipeVariant<SV>;
 };
 
 export interface CVA<V extends VariantsDefinition = VariantsDefinition> {
-    (variant?: Variant<V>, ...className: ClassValue[]): string;
+    (variant?: RecipeVariant<V>, ...className: ClassValue[]): string;
     variantKeys: VariantKeys<V>;
     variantMap: VariantMap<V>;
-    splitProps: <P extends Variant<V>>(props: P) => [Pick<P, keyof V>, Omit<P, keyof V>];
+    splitProps: <P extends RecipeVariant<V>>(props: P) => [Pick<P, keyof V>, Omit<P, keyof V>];
 }
 
 export interface SVA<S extends string = string, SV extends SlotVariantsDefinition<S> = SlotVariantsDefinition<S>> {
-    (variant?: SlotVariant<SV>): { [K in S]: (...className: ClassValue[]) => string };
+    (variant?: RecipeVariant<SV>): { [K in S]: (...className: ClassValue[]) => string };
     variantKeys: VariantKeys<SV>;
     variantMap: VariantMap<SV>;
-    splitProps: <P extends SlotVariant<SV>>(props: P) => [Pick<P, keyof SV>, Omit<P, keyof SV>];
+    splitProps: <P extends RecipeVariant<SV>>(props: P) => [Pick<P, keyof SV>, Omit<P, keyof SV>];
 }
 
 export type TailwindConfigVariantsOptions = {
