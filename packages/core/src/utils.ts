4ab6c7c79666b388e4955fd6804a35c2ed4b99c6
YaredFall <yaredfall@gmail.com>
2026-05-28 00:34:29 +0700

    refactor: introduce shared splitProps function

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,4 +1,4 @@
-import type { SlotVariantsDefinition, VariantKeys, VariantMap, VariantsDefinition } from "./types";
+import type { RecipeVariant, SlotVariantsDefinition, VariantKeys, VariantMap, VariantsDefinition } from "./types";
 
 export function getVariantKeys<V extends VariantsDefinition | SlotVariantsDefinition>(variants: V): VariantKeys<V> {
     return Object.keys(variants);
@@ -8,4 +8,18 @@ export function getVariantMap<V extends VariantsDefinition | SlotVariantsDefinit
     const map = {} as VariantMap<V>;
     for (const key in variants) map[key] = Object.keys(variants[key] ?? {});
     return map;
+}
+
+export function createSplitProps<V extends VariantsDefinition | SlotVariantsDefinition>(
+    variants: V,
+): <P extends RecipeVariant<V>>(props: P) => [Pick<P, keyof V>, Omit<P, keyof V>] {
+    return (props) => {
+        const variantProps = {} as typeof props;
+        const otherProps = {} as typeof props;
+        for (const key in props) {
+            if (variants.hasOwnProperty(key)) variantProps[key] = props[key];
+            else otherProps[key] = props[key];
+        }
+        return [variantProps, otherProps];
+    };
 }
