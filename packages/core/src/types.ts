9fd5d51398fa579c096247515488abef545ba1cc
YaredFall <yaredfall@gmail.com>
2026-05-28 00:54:33 +0700

    refactor!: make recipe utils tree-shakeable (use namespace import instead of function properties)

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -30,21 +30,17 @@ export type SlotRecipe<S extends string = string, SV extends SlotVariantsDefinit
     defaultVariants?: RecipeVariant<SV>;
 };
 
-export interface CVA<V extends VariantsDefinition = VariantsDefinition> {
-    (variant?: RecipeVariant<V>, ...className: ClassValue[]): string;
-    variantKeys: VariantKeys<V>;
-    variantMap: VariantMap<V>;
-    splitProps: <P extends RecipeVariant<V>>(props: P) => [Pick<P, keyof V>, Omit<P, keyof V>];
-}
+export type CVA<V extends VariantsDefinition = VariantsDefinition> = (
+    variant?: RecipeVariant<V>,
+    ...className: ClassValue[]
+) => string;
 
-export interface SVA<S extends string = string, SV extends SlotVariantsDefinition<S> = SlotVariantsDefinition<S>> {
-    (variant?: RecipeVariant<SV>): { [K in S]: (...className: ClassValue[]) => string };
-    variantKeys: VariantKeys<SV>;
-    variantMap: VariantMap<SV>;
-    splitProps: <P extends RecipeVariant<SV>>(props: P) => [Pick<P, keyof SV>, Omit<P, keyof SV>];
-}
+export type SVA<S extends string = string, SV extends SlotVariantsDefinition<S> = SlotVariantsDefinition<S>> = (
+    variant?: RecipeVariant<SV>,
+) => { [K in S]: (...className: ClassValue[]) => string };
 
-export type RecipeVariantProps<V extends CVA | SVA> = Pretty<NonNullable<Parameters<V>[0]>>;
+export type RecipeVariantProps<V extends CVA<VariantsDefinition> | SVA<string, SlotVariantsDefinition<string>>> =
+    Pretty<NonNullable<Parameters<V>[0]>>;
 
 export type TailwindConfigVariantsOptions = {
     rootDir?: string;
