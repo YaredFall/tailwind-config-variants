aa7c26cd780a7a236d9ebba0f0856a495868cb08
YaredFall <yaredfall@gmail.com>
2026-06-01 19:05:05 +0700

    feat: recipes definition hashing

--- packages/core/src/recipes/define.ts
+++ packages/core/src/recipes/define.ts
@@ -1,11 +1,12 @@
+import { hash } from "ohash";
 import type { RecipeDefinition, SlotRecipeDefinition, SlotVariantsDefinition, VariantsDefinition } from "../types";
 
 export function defineRecipe<V extends VariantsDefinition>(recipe: RecipeDefinition<V>) {
-    return Object.assign(recipe, { __type: "cva" });
+    return Object.assign(recipe, { __type: "cva", hash: hash(recipe) }) as RecipeDefinition;
 }
 
 export function defineSlotRecipe<S extends string, SV extends SlotVariantsDefinition<S>>(
     recipe: SlotRecipeDefinition<S, SV>,
 ) {
-    return Object.assign(recipe, { __type: "sva" });
+    return Object.assign(recipe, { __type: "sva", hash: hash(recipe) }) as SlotRecipeDefinition;
 }
