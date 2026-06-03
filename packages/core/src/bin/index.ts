fcb14647fb5d04407b0bfbd31891ff87d208ce64
YaredFall <yaredfall@gmail.com>
2026-06-04 00:00:55 +0700

    chore: add performance log to cli

--- packages/core/src/bin/index.ts
+++ packages/core/src/bin/index.ts
@@ -2,13 +2,18 @@
 import { execute } from "../builder";
 import { loadConfig } from "../config/load";
 import { resolveConfig } from "../config/resolve";
+import * as logger from "../logger";
 import { loadRecipes } from "../recipes/load";
 import { resolveRecipes } from "../recipes/resolve";
+
+const inform = logger.measure();
 
 const configFile = await loadConfig();
 const config = resolveConfig(configFile);
 
 const recipeFiles = await loadRecipes(config.recipes);
 const recipes = resolveRecipes(recipeFiles);
+
+inform(`Resolved ${recipes.size} recipes in {time}`);
 
 execute({ config, recipes });
